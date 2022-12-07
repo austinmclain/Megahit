ea14f37f9a82debf5878af4a903dccd75fbad0ce
Austin <93621884+austinmclain@users.noreply.github.com>
2022-12-06 21:22:25 -0600

    Change query in ProfileRepository

--- backend/src/main/java/com/megahit/backend/Profile/ProfileRepository.java
+++ backend/src/main/java/com/megahit/backend/Profile/ProfileRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProfileRepository extends CrudRepository<Profile, Integer> {
-    @Query("SELECT p.profile_name, p.profile_picture FROM Profile p WHERE p.emailAddress = ?1")
+    @Query(value = "SELECT p FROM Profile p WHERE p.emailAddress = ?1")
     Iterable<Profile> findAllByEmailAddress(String emailAddress);
 }
